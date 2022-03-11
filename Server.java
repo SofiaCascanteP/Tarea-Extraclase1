f7792c8aa43438d1d16bb201023072f04ea0cb35
SofiaCascanteP <sofiacascante18@estudiantec.cr>
2022-03-10 20:44:15 -0600

    Documenté el Server

--- Server.java
+++ Server.java
@@ -17,11 +17,11 @@ public class Server {
                 while(true){
 
                     servicio = servidor.accept();
-                    DataInputStream flujoDatosEntrada =new DataInputStream(servicio.getInputStream());
-                    OutputStream escribir = servicio.getOutputStream();
-                    DataOutputStream flujoDatosSalida = new DataOutputStream(escribir);
+                    DataInputStream flujoDatosEntrada =new DataInputStream(servicio.getInputStream()); //Crea un objeto para recibir mensajes del usuario
+                    OutputStream escribir = servicio.getOutputStream(); //Objeto para mandar a escribir en el cliente
+                    DataOutputStream flujoDatosSalida = new DataOutputStream(escribir); //Aqui se escriben las cosas x
 
-                    Servidor cc = new Servidor(servicio,flujoDatosEntrada,flujoDatosSalida);
+                    Servidor cc = new Servidor(servicio,flujoDatosEntrada,flujoDatosSalida); //Parametros, la conexion y los objetos de escritura/lectura
                     cc.start();
                 }
             }
@@ -32,7 +32,8 @@ public class Server {
         }
     }
     public static void main(String[] args){
-
+         // Se crea una instancia de la clase Servidor
+         
         Server os= new Server();
         os.multiCuenta();
     }
